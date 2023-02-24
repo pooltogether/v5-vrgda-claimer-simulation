946e4140518afccc341873e242e27bc1483b75ce
Brendan Asselstine <mail.asselstine@gmail.com>
2023-02-24 11:44:54 -0800

    Added ability to configure time duration

--- src/simulateVrgdaClaimer.js
+++ src/simulateVrgdaClaimer.js
@@ -34,8 +34,9 @@ const command = async function (options) {
     const decayConstant = Math.log(parseFloat(options.decayPercent))
     const targetPrice = parseFloat(options.targetPrice)
     const count = parseInt(options.count)
+    const duration = parseInt(options.duration)
     const fraction = parseFloat(options.fraction)
-    const linearVrgdaPerTimeUnit = count / (fraction*gasPrices.length)
+    const linearVrgdaPerTimeUnit = count / (fraction*duration)
     const minimumProfit = parseEther(''+options.minimumProfit)
     const claimGas = parseInt(options.claimGas)
 
@@ -64,7 +65,10 @@ const command = async function (options) {
     function computeRevenue(numberOfClaims) {
         let revenue = BigInt(0)
         for (var i = 0; i < numberOfClaims; i++) {
-            revenue += getVrgdaPrice(currentTime, sold + i)
+            const fee = getVrgdaPrice(currentTime, sold + i)
+            // console.log(`checking: ${currentTime}, ${sold + i}, ${fee}`)
+            // process.exit(1);
+            revenue += fee
         }
         return revenue
     }
@@ -72,18 +76,19 @@ const command = async function (options) {
     function computeMaxProfitableClaimCount(gasPrice) {
         const remaining = count - sold
         let chunkSize
-        let iterations = 100
-        if (remaining < 100) {
+        let maxIterations = 10
+        if (remaining < maxIterations) {
             chunkSize = 1
-            iterations = remaining
+            maxIterations = remaining
         } else {
-            chunkSize = parseInt(remaining / 100)
+            chunkSize = parseInt(remaining / maxIterations)
         }
+        // console.log(`chunkSize: ${chunkSize}, maxIterations: ${maxIterations}`)
         let profit = 0;
         let claimCount = 0;
         let cost = 0;
         let revenue = 0;
-        for (let i = 1; i <= iterations; i++) {
+        for (let i = 1; i <= maxIterations; i++) {
             let count = i*chunkSize
             let currentCost = computeCost(count, gasPrice)
             let currentRevenue = computeRevenue(count, gasPrice)
@@ -102,14 +107,17 @@ const command = async function (options) {
 
     let claimHistory = []
     
-    gasPrices.forEach(gasPrice => {
-        const currentPrice = getVrgdaPrice(currentTime, sold)
-        logv(`Checking @${currentTime} sold: ${sold}, gasPrice: ${gasPrice} currentPrice: ${currentPrice}`)
+    for (currentTime = 0; currentTime < duration; currentTime++) {
+        const currentFee = getVrgdaPrice(currentTime, sold)
+        const gasPrice = gasPrices[parseInt(currentTime / duration * gasPrices.length)]
+        // logv(`Checking @${currentTime} sold: ${sold}, gasPrice: ${gasPrice} currentFee: ${currentFee}`)
 
         const [claimCount, profit, cost, revenue] = computeMaxProfitableClaimCount(gasPrice)
+        // log(`count: ${claimCount}, profit: ${profit}, cost: ${cost}, revenue: ${revenue}`)
         // log(`check: ${claimCount}, reamining: ${count - sold} profit: ${formatEther(profit)}, cost: ${formatEther(cost)}`)
         if (claimCount > 0 && profit > minimumProfit) {
-            console.log(chalk.dim(`@${currentTime}: Claimed ${claimCount} with profit ${formatEther(profit)} after cost of ${formatEther(cost)} with cost per claim: ${formatEther(revenue / BigInt(claimCount))}`))
+            sold += claimCount
+            console.log(chalk.dim(`@${currentTime}: Total sold: ${sold}/${count} Claimed: ${claimCount} gasPrice: ${gasPrice} with profit ${formatEther(profit)}. Fee per claim: ${formatEther(revenue / BigInt(claimCount))} gas per claim: ${formatEther(parseUnits(""+gasPrice, 'gwei') * BigInt(claimGas))}`))
             claimHistory.push({
                 time: currentTime,
                 count: claimCount,
@@ -117,13 +125,8 @@ const command = async function (options) {
                 cost,
                 revenue
             })
-            sold += claimCount
         }
-
-
-
-        currentTime++;
-    })
+    }
 
     console.log(chalk.green(`Done! sold ${sold} out of ${options.count}`))
 
@@ -134,10 +137,11 @@ const command = async function (options) {
 program.option('-f, --fraction <number>', 'Fraction of the duration that tickets must be claimed by', 0.5)
 program.option('-v, --verbosity', 'Verbose logging', false)
 program.option('-c, --count <number>', 'The number of claims', 2000)
-program.option('-d, --decayPercent <number>', 'The percentage rate of price change per unit time', 1.3)
-program.option('-t, --targetPrice <number>', 'The target price', 0.00000001)
+program.option('-d, --decayPercent <number>', 'The percentage rate of price change per unit time', 1.01)
+program.option('-du, --duration <number>', 'The time duration over which to run the simulation', 100)
+program.option('-t, --targetPrice <number>', 'The target price', 0.0000001)
 program.option('-cg, --claimGas <number>', 'The gas usage of each claim transaction', 200_000)
-program.option('-m, --minimumProfit <number>', 'The minimum claim profit in ether', 0.001)
+program.option('-m, --minimumProfit <number>', 'The minimum claim profit in ether', 0.000005)
 program.requiredOption('-gf, --gasFile <filepath>', 'CSV to use for gas prices, where each row is <gas gwei integer>')
 
 program.action(command)
